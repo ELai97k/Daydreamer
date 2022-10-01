dce1b2af05243be1bd0d97775e47274d13fa2773
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-10-01 23:08:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,76 @@ from discord.ext import commands
 intents = discord.Intents.default().all()
 intents.members = True
 
-client = commands.Bot(command_prefix="?", case_insensitive=True, intents=intents)
+# custom help command
+class CustomHelpCommand(commands.HelpCommand):
+    color=0xffd966
+    def get_ending_note(self):
+        return 'Use {0}{1} [command] for more info on a command.'.format(self.clean_prefix, self.invoked_with)
+
+    def get_command_signature(self, command):
+        parent = command.full_parent_name
+        if len(command.aliases) > 0:
+            aliases = '|'.join(command.aliases)
+            fmt = '[%s|%s]' % (command.name, aliases)
+            if parent:
+                fmt = parent + ' ' + fmt
+            alias = fmt
+        else:
+            alias = command.name if not parent else parent + ' ' + command.name
+
+        return '%s%s %s' % (self.clean_prefix, alias, command.signature)
+
+    # cogs and commands
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title='Commands', color=0xffd966)
+        description = self.context.bot.description
+        if description:
+            embed.description = description
+
+        for cog, commands in mapping.items():
+            if cog is not None:
+                name = cog.qualified_name
+                filtered = await self.filter_commands(commands, sort=True)
+
+                if filtered:
+                    value = '\u2002 '.join('`' + c.name + '`' for c in commands if not c.hidden)
+
+                    if cog and cog.description:
+                        value = '{0}\n{1}'.format(cog.description, value)
+
+                    embed.add_field(name=name, value=value, inline=False)
+
+        await self.get_destination().send(embed=embed)
+
+    # cog info
+    async def send_cog_help(self, cog):
+        embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog), color=0xffd966)
+        if cog.description:
+            embed.description = cog.description
+
+        filtered = await self.filter_commands(cog.get_commands(), sort=True)
+        for command in filtered:
+            embed.add_field(name=self.get_command_signature(command), value=command.short_doc or '...', inline=False)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    # command info
+    async def send_command_help(self, command):
+        embed = discord.Embed (
+            color=0xffd966
+        )
+        embed.add_field (
+            name=f"{command.name}",
+            value=command.help,
+            inline=False
+        )
+        await self.get_destination().send(embed=embed)
+
+client = commands.Bot(command_prefix="?", 
+help_command=CustomHelpCommand(), 
+case_insensitive=True, 
+intents=intents)
 
 # cogs
 async def load_extensions():
