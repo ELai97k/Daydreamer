7118e960099e10382e292a1934bdc5e0c5a6f980
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-10-06 18:42:18 +0800

    Update auto_responses.py

--- cogs/auto_responses.py
+++ cogs/auto_responses.py
@@ -94,3 +94,6 @@ class Auto_Responses(commands.Cog):
 
 async def setup(client):
     await client.add_cog(Auto_Responses(client))
+
+async def teardown(client):
+    await client.remove_cog(Auto_Responses(client))
