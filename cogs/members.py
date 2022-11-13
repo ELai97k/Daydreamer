490dc353085d2a09d58b6599a07b8b0d206d8d62
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-11-13 16:17:32 +0800

    Create members.py

---
+++ cogs/members.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+class Members(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(pass_context=True)
+    async def pfp(self, ctx, *, member:discord.Member):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+
+        if member is None:
+            member = ctx.author
+
+        embed = discord.Embed(color=discord.Color.blurple)
+        embed.set_image(url='{}'.format(member.avatar.url))
+
+        await ctx.send(embed=embed)
+
+
+async def setup(client):
+    await client.add_cog(Members(client))
+
+async def teardown(client):
+    await client.remove_cog(Members(client))
