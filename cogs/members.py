238b2f17741be4978984fa67819c188f42661271
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-11-13 16:51:14 +0800

    Update members.py

--- cogs/members.py
+++ cogs/members.py
@@ -5,7 +5,7 @@ class Members(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, help="Command to show user's profile picture (pfp)")
     async def pfp(self, ctx, *, user:discord.Member=None):
         if ctx.author == self.client.user:
             return
@@ -17,6 +17,23 @@ class Members(commands.Cog):
 
         embed = discord.Embed(color=discord.Color.blurple())
         embed.set_image(url=user.avatar.url)
+
+        await ctx.send(embed=embed)
+
+
+    @commands.command(help="Command to fetch number of members in server.")
+    async def server_members(self, ctx):
+        if ctx.author == self.client.user:
+            return
+        if ctx.author.bot:
+            return
+
+        embed = discord.Embed (
+            title = f"{ctx.guild.name} members",
+            description = f"👥 Members: {ctx.guild.member_count}",
+            color=discord.Color.blurple()
+        )
+        embed.add_field(name="Member names:", value=f"{ctx.get_all_members()}")
 
         await ctx.send(embed=embed)
 
