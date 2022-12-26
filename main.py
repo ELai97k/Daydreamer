1c73586ea8eb71e463bfe7b974c55d607af02dca
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-12-26 12:25:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 import os
-import asyncio
 from discord.ext import commands
 
 intents = discord.Intents.default().all()
@@ -61,14 +60,9 @@ case_insensitive=True,
 intents=intents)
 
 # cogs
-async def load_extensions():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            await client.load_extension(f'cogs.{filename [:-3]}')
-
-async def main():
-    await load_extensions()
-asyncio.run(main())
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f'cogs.{filename [:-3]}')
 
 # on ready event
 @client.event
